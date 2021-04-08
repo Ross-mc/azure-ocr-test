59aaa5773991c530675ea1d7435e79cba7293400
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-08 19:00:19 +0100

    front end takes image, converts to base64, sends to cloudinary, url is sent to OCR for reading, send result to client

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -20,13 +20,7 @@ import { useImperativeHandle, useState } from "react";
 
 const ImageUpload = () => {
   const [images, setImages] = useState([]);
-
-  //###############################################################################################
-
-  // create a method of uploading to blob storage, getting the url and then calling the back end with the url
-
-  //###############################################################################################
-
+  //helper function to convert image to base64
   const fileToDataUri = (image) => {
     return new Promise((res) => {
       const reader = new FileReader();
@@ -43,176 +37,7 @@ const ImageUpload = () => {
     });
   };
 
-  // function makeblob(b64Data, contentType, sliceSize) {
-  //   contentType = contentType || "";
-  //   sliceSize = sliceSize || 512;
 
-  //   var byteCharacters = atob(b64Data);
-  //   var byteArrays = [];
-
-  //   for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-  //     var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-  //     var byteNumbers = new Array(slice.length);
-  //     for (var i = 0; i < slice.length; i++) {
-  //       byteNumbers[i] = slice.charCodeAt(i);
-  //     }
-
-  //     var byteArray = new Uint8Array(byteNumbers);
-
-  //     byteArrays.push(byteArray);
-  //   }
-
-  //   var blob = new Blob(byteArrays, { type: contentType });
-  //   return blob;
-  // }
-
-  // // $("#myImage").change(function () {
-  // //   //Load everything in
-  // //   var reader = new FileReader();
-  // //   var file = this.files[0];
-  // //   //  var mb = $(this).serializeObject();
-  // //   console.log(file);
-  // //   reader.onload = function () {
-  // //     var resultData = this.result;
-
-  // //     //     console.log(resultData);
-
-  // //     resultData = resultData.split(",")[1];
-
-  // //     processImage(resultData);
-  // //     // processImage(mb);
-  // //   };
-
-  // //   reader.readAsDataURL(file);
-  // // });
-
-  // const uploadImage = async (e) => {
-  //   console.log(e.target.files[0])
-  //   var data = new FormData()
-  //   data.append('file', e.target.files[0])
-  //   console.log(data.entries())
-  //   if (e.target.files && e.target.files.length > 0) {
-  //     const newImagesPromises = [];
-  //     for (let i = 0; i < e.target.files.length; i++) {
-  //       newImagesPromises.push(fileToDataUri(e.target.files[i]));
-  //     }
-  //     const newImages = await Promise.all(newImagesPromises);
-  //     setImages([...images, ...newImages]);
-  //     const response = await fetch(newImages[0].base64);
-  //     const blob = await response.blob();
-  //     // const file = new File([blob], "capture.png", {
-  //     //   type: "image/png",
-  //     // });
-  //     // call the API
-  //     const data = {
-  //       "url": "https://www.patriotsoftware.com/wp-content/uploads/2019/12/invoice-vs.-receipt-image-of-receipt.jpg"
-  //     }
-  //     fetch(
-  //      
-  //       {
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           "Ocp-Apim-Subscription-Key": 
-  //         },
-  //         method: "POST",
-  //         body: data
-  //       }
-  //     )
-  //       .then((res) => res.json())
-  //       .then((result) => console.log("im the result", result));
-  //   }
-  // };
-
-  // // const uploadImage = (e) => {
-  // //   console.log('i am in the upload image func')
-  // //   const reader = new FileReader();
-  // //   reader.onload = function () {
-  // //     console.log('i am in the onload func')
-  // //     const arrayBuffer = this.result;
-  // //     const array = new Uint8Array(arrayBuffer);
-  // //     const key = ;
-  // //     const uri = ;
-  // //     fetch(uri, {
-  // //       method: "POST",
-  // //       headers: {
-  // //         "Content-Type": "application/octet-stream",
-  // //
-  // //       },
-  // //       data: arrayBuffer,
-  // //       processData: false
-  // //     }).then(res => res.json()).then(res2 => console.log(res2))
-  // //   }
-  // //   reader.readAsArrayBuffer(this.files[0])
-  // // }
-
-  // //#################################################
-  // //$(document).ready(function () {
-
-  // //Step 1. Hook into the myFile input file change event
-
-  // //    var subKey = '[your key]';
-
-  // //     function makeblob(b64Data, contentType, sliceSize) {
-  // //         contentType = contentType || '';
-  // //         sliceSize = sliceSize || 512;
-
-  // //         var byteCharacters = atob(b64Data);
-  // //         var byteArrays = [];
-
-  // //         for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-  // //             var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-  // //             var byteNumbers = new Array(slice.length);
-  // //             for (var i = 0; i < slice.length; i++) {
-  // //                 byteNumbers[i] = slice.charCodeAt(i);
-  // //             }
-
-  // //             var byteArray = new Uint8Array(byteNumbers);
-
-  // //             byteArrays.push(byteArray);
-  // //         }
-
-  // //         var blob = new Blob(byteArrays, { type: contentType });
-  // //         return blob;
-  // //     }
-
-  // //     processImage = function(binaryImage) {
-
-  // //      //   var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
-  // //         var uriBase = "https://eastus.api.cognitive.microsoft.com/vision/v1.0/analyze";
-
-  // //         //    // Request parameters.
-  // //         var params = {
-  // //             "visualFeatures": "Categories,Description,Color",
-  // //             "details": "",
-  // //             "language": "en",
-  // //         };
-
-  // //         $.ajax({
-  // //             url: "https://eastus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories&language=en",
-
-  // //            method: "POST",
-  // //            type: "POST",
-  // //             beforeSend: function (xhrObj) {
-  // //                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subKey);
-
-  // //             },
-  // //             contentType: "application/octet-stream",
-  // //             mime: "application/octet-stream",
-  // //             data: makeblob(binaryImage, 'image/jpeg'),
-  // //             cache: false,
-  // //             processData: false
-
-  // //         }) .done(function(data) {
-  // //    // Show formatted JSON on webpage.
-  // //    $("#responseTextArea").val(JSON.stringify(data, null, 2));
-  // // })
-
-  // //     }
-  // // });
-
-  // //#################################################
 
   const uploadImage = async (e) => {
     if (e.target.files && e.target.files.length > 0) {
@@ -221,126 +46,25 @@ const ImageUpload = () => {
         newImagesPromises.push(fileToDataUri(e.target.files[i]));
       }
       const newImages = await Promise.all(newImagesPromises);
-      console.log("new images", newImages);
+      //grab all the images, save them to state
       setImages([
         ...images,
         ...newImages.filter((image) => image != undefined),
       ]);
-      const testFile = await fetch(newImages[0].base64);
-      const file = new File([testFile.blob()], "test.png", {
-        type: 'image/png'
-      });
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", "docs_upload_example_us_preset")
-      fetch("https://api.cloudinary.com/v1_1/demo/image/upload", {
+      // send the base 64 encoded string to our api
+      fetch("/api/image", {
         method: "post",
-        body: formData
-      }).then(res => res.json()).then(results => console.log(results))
-      // const blobService = createBlobService("DefaultEndpointsProtocol=https;AccountName=reducewastestorage;AccountKey=v8KoVU/bxyzlywv1fHmFvsJao/M4RBKbYXBY1L7nsXGeiDx6sVPRFx1TdXVq0UDsoPHQ8H9xSbZqnBgHsKlinQ==;EndpointSuffix=core.windows.net")
-      // blobService.createAppendBlobFromStream("reducewastecontainer", "testBlob", newImages[0].base64, newImages[0].base64.length, (err, result, response) => {
-      //   if (err){
-      //     console.log(err)
-      //   } else {
-      //     console.log(response)
-      //     console.log(result)
-      //   }
-      // })
-
-      const data = JSON.stringify({
-        url: "https://i2-prod.manchestereveningnews.co.uk/incoming/article19522330.ece/ALTERNATES/s1200/2_CD16940913.jpg"
-      })
-      fetch("/api/ocr", {
-        method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: data
+        body: JSON.stringify({
+          image: newImages[0].base64
+        })
       }).then(res => res.json()).then(results => console.log(results))
+ 
     }
   }
-  // blob sas token
-  // sp=r&st=2021-04-08T14:53:46Z&se=2021-05-15T22:53:46Z&spr=https&sv=2020-02-10&sr=c&sig=KWa3%2BFJGHclFQO407O8I8cmqGNUeApZ9mRlVrgNIexw%3D
-  //blob url
-  //https://reducewastestorage.blob.core.windows.net/reducewastecontainer?sp=r&st=2021-04-08T14:53:46Z&se=2021-05-15T22:53:46Z&spr=https&sv=2020-02-10&sr=c&sig=KWa3%2BFJGHclFQO407O8I8cmqGNUeApZ9mRlVrgNIexw%3D
-  //BLOB_KEY=v8KoVU/bxyzlywv1fHmFvsJao/M4RBKbYXBY1L7nsXGeiDx6sVPRFx1TdXVq0UDsoPHQ8H9xSbZqnBgHsKlinQ==
-  //STORAGE_ACCOUNT=DefaultEndpointsProtocol=https;AccountName=reducewastestorage;AccountKey=v8KoVU/bxyzlywv1fHmFvsJao/M4RBKbYXBY1L7nsXGeiDx6sVPRFx1TdXVq0UDsoPHQ8H9xSbZqnBgHsKlinQ==;EndpointSuffix=core.windows.net
 
-      //   const file = new File([result], "capture.png", {
-      //     type: 'image/png'
-      // });
-      // console.log(file)
-      //   const fd = new FormData();
-      //   fd.append('image', file);
-      //   fetch(, {
-      //     // headers: {
-      //     //   "Content-Type": "application/json",
-      //     // },
-      //     method: "POST",
-      //     data: fd,
-      //     processData: false,
-      //     contentType: false
-      //   });
-      // const reader = new FileReader();
-      // reader.addEventListener("loadend", () => {
-      //   const formData = new FormData()
-      // })
-      // })
-
-  // //     //     let reader = new FileReader();
-
-  // //     // reader.addEventListener("loadend", function () {
-
-  // //     //   formData = new FormData();
-  // //     //   formData.append("content", reader.result);
-  // //     //   fetch("https://wiki.epfl.ch/test.php", { method: 'POST', body: formData });
-  // //     //   reader.removeEventListener("loadend");
-
-  // //     // });
-
-  // //     // reader.readAsDataURL(content);
-
-  // //     // .then((res) => res.json())
-  // //     // .then((result) => console.log("im the result", result));
-  // //   }
-  // //   // e.target.value = "";
-  // // };
-
-  // // converts image to base64, we call atob to convert base64 to binary
-  // // const fileToDataUri = (image) => {
-  // //   return new Promise((res) => {
-  // //     const reader = new FileReader();
-  // //     const { type, name, size } = image;
-  // //     reader.addEventListener("load", () => {
-  // //       res({
-  // //         base64: reader.result,
-  // //         name: name,
-  // //         type,
-  // //         size: size,
-  // //       });
-  // //     });
-  // //     reader.readAsDataURL(image);
-  // //   });
-  // // };
-
-  // // const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
-  // //   const byteCharacters = atob(b64Data);
-  // //   const byteArrays = [];
-
-  // //   for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-  // //     const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-  // //     const byteNumbers = new Array(slice.length);
-  // //     for (let i = 0; i < slice.length; i++) {
-  // //       byteNumbers[i] = slice.charCodeAt(i);
-  // //     }
-
-  // //     const byteArray = new Uint8Array(byteNumbers);
-  // //     byteArrays.push(byteArray);
-  // //   }
-
-  // //   const blob = new Blob(byteArrays, {type: contentType});
-  // //   return blob;
 
   return (
     <input
--- server.js
+++ server.js
@@ -1,56 +1,40 @@
 
 const express = require('express');
 const postToOCR = require("./ocr");
-const multer = require("multer");
-const MulterAzureStorage = require("multer-azure-storage")
-
-//
-// var multer = require('multer')
-// var MulterAzureStorage = require('multer-azure-storage')
-// var upload = multer({
-//   storage: new MulterAzureStorage({
-//     azureStorageConnectionString: 'https://mystorageaccount.blob.core.windows.net/',
-//     azureStorageAccessKey: 'myaccesskey',
-//     azureStorageAccount: 'mystorageaccount',
-//     containerName: 'photos',
-//     containerSecurity: 'blob'
-//   })
-// })
-
-//
-
-const upload = multer({
-  storage: new MulterAzureStorage({
-        azureStorageConnectionString: "DefaultEndpointsProtocol=https;AccountName=reducewastestorage;AccountKey=v8KoVU/bxyzlywv1fHmFvsJao/M4RBKbYXBY1L7nsXGeiDx6sVPRFx1TdXVq0UDsoPHQ8H9xSbZqnBgHsKlinQ==;EndpointSuffix=core.windows.net",
-    azureStorageAccessKey: 'v8KoVU/bxyzlywv1fHmFvsJao/M4RBKbYXBY1L7nsXGeiDx6sVPRFx1TdXVq0UDsoPHQ8H9xSbZqnBgHsKlinQ==',
-    azureStorageAccount: 'reducewastestorage',
-    containerName: 'reducewastecontainer'
-  })
-})
+const cloudinary = require("cloudinary").v2;
 
 
-//'https://reducewastestorage.blob.core.windows.net'
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API,
+  api_secret: process.env.CLOUD_SECRET
+}); 
+
+
 const PORT = 3001;
 
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
 
-app.post("/api/image", upload.single('file'), async (req, res) => {
-  // console.log(req.body)
-  console.log(res)
-  // console.log(results)
+app.post("/api/image", async (req, res) => {
+  //we take the base64 string and upload to cloudinary
+  cloudinary.uploader.upload(req.body.image,{tags: "test"}, async (err, img) => {
+    if (err){
+      console.log(err)
+    } else {
+      // we get the url returned from cloudinary and send to OCR, this takes a few seconds to complete
+      const resultFromOcr = await postToOCR(img.url);
+      res.status(200).json(resultFromOcr)
+    }
+  })
 })
 
 app.post("/api/ocr", async (req, res) => {
-  // console.log(req.body)
-  const test = await postToOCR(req.body.url);
-  res.status(200).json(test)
-  // console.log(results)
+
+  res.status(200).json("currently  not submitting request to azure")
 })
 
 
